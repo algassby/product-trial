c6bc061817b7cd88d4c848ac92663fd1ecfc351d
algassby <algassby@hotmail.com>
2025-02-17 18:38:05 +0100

    updated AuditorAware for Jpa auditing

--- back/src/main/java/com/barry/product/audit/AuditorAwareImpl.java
+++ back/src/main/java/com/barry/product/audit/AuditorAwareImpl.java
@@ -1,6 +1,7 @@
 package com.barry.product.audit;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -10,7 +11,11 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-      git   
-        return Optional.of("system"); // Remplace "system" par le vrai utilisateur si nécessaire
+
+        var context = SecurityContextHolder.getContext();
+        if (context != null && context.getAuthentication() != null && context.getAuthentication().getName() != null) {
+            return  Optional.of(context.getAuthentication().getName());
+        }
+        return Optional.of("Anonymous");
     }
 }
